c4d1cf0dff356c0ed4b54b638a86a8d2e76f1507
AhmedMuhammedin <ahmedmahmoudmuhammedin@gmail.com>
2023-08-26 00:30:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>Ahmed Mahmoud</title>
+    <title>Ahmed Mahmoud Sub Domain</title>
 </head>
 <body>
     <div class="name-container">
-        <h1>Ahmed Mahmoud</h1>
+       <h1>Ahmed Mahmoud Muhammedin</h1>
+       <a href="https://www.facebook.com/AhmedMahmoudMuhammedin">facebook</a>
+       <a href="https://www.linkedin.com/in/ahmedmuhammedin/">linkedin</a>
+       <a href="https://medium.com/@ahmedmahmoudmuhammedin">medium</a>
     </div>
 </body>
 </html>
