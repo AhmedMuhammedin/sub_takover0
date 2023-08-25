5e0e2cf7a00a28876e18c02990046431169448e0
AhmedMuhammedin <ahmedmahmoudmuhammedin@gmail.com>
2023-08-26 00:09:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Ahmed Mahmoud</title>
-</head>
-<body>
-    <div class="name-container">
-        <h1>Ahmed Mahmoud</h1>
-    </div>
-</body>
-</html>
+<?php
+    include('connection.php');
+    if (isset($_POST['submit'])) {
+        $username = $_POST['user'];
+        $password = $_POST['pass'];
+
+        $sql = "select * from login where username = '$username' and password = '$password'";  
+        $result = mysqli_query($conn, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+        
+        if($count == 1){  
+            header("Location: welcom.php");
+        }  
+        else{  
+            echo  '<script>
+                        window.location.href = "index.php";
+                        alert("Login failed. Invalid username or password!!")
+                    </script>';
+        }     
+    }
+    ?>
